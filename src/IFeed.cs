6de77f93f440f99a8bcf47db65a2062a90bff76c
wdcossey <wdcossey@github.com>
2016-11-22 19:01:13 +0200

    Added IFeed interface

--- src/IFeed.cs
+++ src/IFeed.cs
@@ -1,58 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dng.Syndication.Attributes;
 
 namespace dng.Syndication
 {
-    public class Feed : IFeed
+    internal interface IFeed
     {
         /// <summary>
         /// Name for the feed.
         /// </summary>
-        public string Title { get; set; }
+        string Title { get; set; }
 
         /// <summary>
         /// Author is used to specify the e-mail address of the author of an item
         /// </summary>
-        public Author Author { get; set; }
+        Author Author { get; set; }
 
         /// <summary>
         /// Defines the hyperlink to the channel
         /// </summary>
-        public Uri Link { get; set; }
+        Uri Link { get; set; }
 
         /// <summary>
         /// Defines the last-modified date of the content of the feed
         /// </summary>
-        public DateTime UpdatedDate { get; set; }
+        DateTime UpdatedDate { get; set; }
 
         /// <summary>
         /// Each FeedEntries element defines an article or "story" in an RSS feed.
         /// </summary>
-        public IList<IFeedEntry> FeedEntries { get; set; }
+        IList<IFeedEntry> FeedEntries { get; set; }
 
         /// <summary>
         /// Specifies the language the feed is written in
         /// </summary>
-        public string Language { get; set; }
+        string Language { get; set; }
 
         /// <summary>
         /// The copyright child element notifies about copyrighted material.
         /// </summary>
-        public string Copyright { get; set; }
+        string Copyright { get; set; }
 
         /// <summary>
         /// Defines the publication date for the content of the feed
         /// </summary>
-        public DateTime PublishedDate { get; set; }
+        DateTime PublishedDate { get; set; }
 
         /// <summary>
         /// Specifies the program used to generate the feed
         /// </summary>
-        public string Generator { get; set; }
+        string Generator { get; set; }
 
         /// <summary>
         /// Describes the feed
         /// </summary>
-        public string Description { get; set; }
+        string Description { get; set; }
     }
 }
