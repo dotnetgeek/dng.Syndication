975f92ee55fc35e802d33bfbca79688fba30c31d
Daniel Müller <dm@telligene-consulting.com>
2017-10-24 00:36:50 +0200

    fix merge issue

--- src/Enclosure.cs
+++ src/Enclosure.cs
@@ -2,14 +2,11 @@
 {
     public class Enclosure
     {
-<<<<<<< HEAD
         public Enclosure()
         {
             Length = 0;
         }
 
-=======
->>>>>>> 173348ac0538654dbb2c584a392fa3ed4b52d57d
         public string Url { get; set; }
 
         public int Length { get; set; }
